ee29e4de71b417dcab5883f36375ac71198ff183
eniallator <eniallatormc@gmail.com>
2016-08-12 15:26:47 +0100

    Added a map format and have it displaying, next will need to make a map grid and also add collision detection with solid blocks only.

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
 function inputControl()
-  -- just seeing if the up key was pressed rather than still down
   upIsDown = false
 
   if love.keyboard.isDown("up") then
@@ -12,8 +11,10 @@ function inputControl()
     stillDown = false
   end
 
-  if upIsDown then
+  -- jumpsLeft idea from cran :)
+  if upIsDown and jumpsLeft and jumpsLeft > 0 then
     player.vel.y = player.vel.y + jumpHeight
+    jumpsLeft = jumpsLeft - 1
   end
 
   if love.keyboard.isDown("right") then
@@ -37,20 +38,42 @@ function updatePos()
   -- Collision detection with the ground
   if player.pos.y + 20 + player.vel.y < screenDim.y then
     player.pos.y = player.pos.y + player.vel.y
+
   else
+    jumpsLeft = 2
     player.vel.y = 0
   end
 end
 
+function displayMap()
+  for coords, data in pairs(map) do
+    local xNum = coords:find("x")
+    local yNum = coords:find("y")
+
+    local blockX = tonumber(coords:sub(xNum +1, yNum -1))
+    local blockY = tonumber(coords:sub(yNum +1, #coords))
+
+    local blockData = blocks[data.block]
+    love.graphics.setColor(blockData.r, blockData.g, blockData.b)
+    love.graphics.rectangle("fill", (blockX-1)*blockSize, (blockY-1)*blockSize, data.w*blockSize, data.h*blockSize)
+  end
+end
+
 function love.load()
-  -- Loading vars
   screenDim = {}
   screenDim.x, screenDim.y = love.graphics.getDimensions()
-  player = {pos = {x = 1, y = 1}, vel = {x = 0, y = 0}}
+  player = {pos = {x = 1, y = 1}, vel = {x = 0, y = 0}, col = {r = 0, g = 0, b = 255}}
   moveSpeed = 0.8
   jumpHeight = -5
-  gravity = 0.9
+  gravity = 3
   drag = 0.93
+  blockSize = screenDim.y/20
+
+  -- For now just using RGB colours and only using "stone" but will add more blocks and also will add textures later
+  blocks = {stone = {r = 150, b = 150, g = 150}}
+
+  -- Test map for now but it's working perfectly and map creation shouldn't be very difficult based off of this
+  map = {x10y16 = {block = "stone", w = 8, h = 1}, x5y6 = {block = "stone", w = 2, h = 2}, x21y6 = {block = "stone", w = 2, h = 2}}
 end
 
 function love.update()
@@ -59,5 +82,7 @@ function love.update()
 end
 
 function love.draw()
+  love.graphics.setColor(player.col.r, player.col.g, player.col.b)
   love.graphics.rectangle("fill", player.pos.x, player.pos.y, 20, 20)
+  displayMap()
 end
