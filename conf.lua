09ac01e046cf3e1bdcef5a9c7da430619349acfd
eniallator <eniallatormc@gmail.com>
2016-10-23 19:38:09 +0100

    Added icon game icon to display instead of stone texture

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
   t.window.resizable = true
-  t.window.icon = "assets/textures/blocks/stone.png"
+  t.window.icon = "assets/textures/icons/platformer.png"
   t.title = "Platformer Game"
 end
